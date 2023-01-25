a64609dd686dcdcbc44e11f1f7a3fd2388e1c4e2
Samuel <201501502@rajalakshmi.edu.in>
2023-01-25 12:41:12 +0530

    Update

--- app.py
+++ app.py
@@ -1,61 +1,34 @@
-from flask import *  
-import os 
+from flask import Flask, render_template, request, redirect
 import qrcode
-import uuid
+import json
+import redis
 
-qr_codes={}
-
-
-
-app = Flask(__name__) 
-
+app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def default():
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data("http://192.168.28.59:5000")
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+    img_file = 'static/qr.png'
+    img.save(img_file)
+    return render_template('index.html', img_file=img_file)
 
-@app.route('/upload',methods=["POST","GET"])
-def getData():
-    if request.method == 'POST':
-        result = request.files['file']
-        color = request.form.get('color')
-        side = request.form.get('side')
-        quantity = request.form.get('quantity')
-        #print(color, side, quantity)
-        result.save(result.filename)
-        #os.remove(result.filename)
-        # Create a unique identifier
-        unique_id = str(uuid.uuid4())
+@app.route('/update_state', methods=['POST'])
+def update_state():
+    state = request.form['state']
+    r.set('device_state', state)
+    return redirect('/redirect')
 
-        # Create the data to be encoded in the QR code, including the unique identifier
-        #data = color + side + quantity + " data recieved + unique code next " + unique_id
-        # Create the QR code instance
-        qr = qrcode.QRCode(version=1, box_size=10, border=5)
-        data="http://192.168.28.59:5000/scan/" + unique_id
-        # Add the data to the QR code
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        # Create an image from the QR code instance
-        img = qr.make_image(fill_color="black", back_color="white")
-
-        # Save the image
-        img.save("static/qr.png")   
-        # Add the identifier to the qr_codes dictionary
-        qr_codes[unique_id] = True
-
-    return render_template('payment.html', qr_code_id=unique_id, data=data)
-
-@app.route("/scan/<qr_code_id>")
-def scan_qr_code(qr_code_id):
-    # Check if the scanned QR code's identifier exists in the qr_codes dictionary
-    if qr_codes.get(qr_code_id):
-        # remove the scanned qr code from the dictionary
-        qr_codes.pop(qr_code_id)
-        data="http://192.168.1.15:5000/completed.html"
-        return render_template("completed.html", url=data)
-    else:
-        return "Invalid QR code or already scanned"
+@app.route('/redirect')
+def redirect_route():
+    state = r.get('device_state').decode("utf-8")
+    if state == "scan":
+        return render_template('scan.html')
+    elif state == "redirect":
+        return render_template('redirect.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",use_reloader=False)
+    app.run(debug=True, host="0.0.0.0")
